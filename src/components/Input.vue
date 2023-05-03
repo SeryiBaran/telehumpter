276c09e487cc11ad3339fecec135d709051aad6e
SeryiBaran <seryibaranminepy@yandex.ru>
2023-05-03 15:29:06 +0300

    input error transition && input border and outline color transition

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { toRef } from 'vue'
+import { computed, ref, toRef, watch } from 'vue'
 import { useField } from 'vee-validate'
 
 const props = withDefaults(defineProps<{
@@ -16,20 +16,51 @@ const name = toRef(props, 'name')
 const { value, errorMessage, errors } = useField(props.name, undefined, {
   initialValue: props.initialValue,
 })
+
+const isValid = computed(() => errors.value.length === 0)
+
+const persistedErrorMessage = ref('')
+
+watch(errorMessage, (newMessage) => {
+  if (newMessage && newMessage?.trim() !== '')
+    persistedErrorMessage.value = newMessage
+})
 </script>
 
 <template>
   <div class="wrapper">
     <input
-      v-model="value" :name="name" type="text" :placeholder="props.placeholder" class="input input-bordered"
-      :class="{ 'input-error': errors.length > 0 }"
+      v-model="value" :name="name" type="text" :placeholder="props.placeholder" class="outline-transparent input input-bordered"
+      :class="{ 'input-error': !isValid }"
     >
-    <span class="text-error text-xs">{{ errorMessage }}</span>
+
+    <span class="text-error text-xs">
+      <Transition name="errorTransition">
+        <span v-if="!isValid" class="block">{{ persistedErrorMessage }}</span>
+      </Transition>
+    </span>
   </div>
 </template>
 
 <style scoped>
 .wrapper {
   @apply flex flex-col gap-1 min-h-[5rem];
+}
+
+.input {
+  transition-property: outline-color, border-color;
+  transition-duration: 200ms;
+  transition-timing-function: ease-in-out;
+}
+
+.errorTransition-enter-active,
+.errorTransition-leave-active {
+  transition: all 200ms ease-in-out;
+}
+
+.errorTransition-enter-from,
+.errorTransition-leave-to {
+  opacity: 0;
+  transform: translateY(-1rem);
 }
 </style>
