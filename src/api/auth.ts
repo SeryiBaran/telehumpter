4d4f09e8821480fc7d8d151059c6be89b2244856
SeryiBaran <seryibaranminepy@yandex.ru>
2023-05-31 20:25:39 +0300

    improve register script

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -4,10 +4,13 @@ import router from '@/router'
 import { RoutesPaths } from '@/router/routes'
 import type { LoginForm, RegisterForm } from '@/types'
 
+// TODO: пофиксить успешную регистрацию при указании зарегестрированного email
+// Почему-то data.error null
 export async function register(values: RegisterForm) {
   const auth = supabase.auth
-  try {
-    const data = await auth.signUp({
+
+  return auth
+    .signUp({
       email: values.email,
       password: values.password,
       options: {
@@ -16,18 +19,40 @@ export async function register(values: RegisterForm) {
         },
       },
     })
-    if (data.data.user)
-      await supabase
-        .from('profiles')
-        .insert({ id: data.data.user.id, username: values.username })
-    else throw data.error || new Error('data.user после регистрации пуст!')
-    toast.success('Регистрация прошла успешно! Не забудьте подтвердить Email!')
-    router.push(RoutesPaths.LOGIN)
-  } catch (error) {
-    toast.error('Что-то пошло не так! Ошибка в консоли браузера.')
+    .then(async (data) => {
+      console.log(data)
+      if (!data.error) {
+        if (data.data.user)
+          await supabase
+            .from('profiles')
+            .insert({ id: data.data.user.id, username: values.username })
+      } else {
+        throw data.error || new Error('Что-то случилось...')
+      }
+    })
+    .then(() => {
+      toast.success(
+        'Регистрация прошла успешно! Не забудьте подтвердить Email!'
+      )
 
-    console.error(error)
-  }
+      router.push(RoutesPaths.LOGIN)
+
+      return true
+    })
+    .catch((error: Error & { status: string }) => {
+      const message = error.message
+
+      if (message.includes('exists')) {
+        toast.error('Email уже зарегестрирован!')
+      } else if (message.includes('invalid')) {
+        toast.error('Неверные данные!')
+      } else {
+        toast.error('Что-то пошло не так! Ошибка в консоли браузера.')
+      }
+
+      console.error(error)
+      return false
+    })
 }
 
 export async function loginWithPassword(values: LoginForm) {
@@ -38,19 +63,7 @@ export async function loginWithPassword(values: LoginForm) {
       password: values.password,
     })
     .then((data) => {
-      if (data.error) {
-        const lowerCasedError = data.error.message.toLowerCase()
-
-        if (lowerCasedError.includes('confirm')) {
-          toast.error('Email не подтвержден!')
-        } else if (lowerCasedError.includes('invalid')) {
-          toast.error('Неверные данные!')
-        } else {
-          toast.error('Что-то пошло не так! Ошибка в консоли браузера.')
-        }
-
-        console.error(data.error)
-      } else {
+      if (!data.error) {
         toast.success('Вход выполнен!')
 
         const currentRoute = router.currentRoute.value
@@ -61,7 +74,26 @@ export async function loginWithPassword(values: LoginForm) {
           redirectTo = currentRoute.query.redirect
 
         router.push(redirectTo)
+      } else {
+        throw data.error
       }
+    })
+    .catch((error: Error & { status: string }) => {
+      const message = error.message
+      const status = error.status
+
+      console.error(message)
+      console.error(status)
+
+      if (message.includes('confirm')) {
+        toast.error('Email не подтвержден!')
+      } else if (message.includes('invalid')) {
+        toast.error('Неверные данные!')
+      } else {
+        toast.error('Что-то пошло не так! Ошибка в консоли браузера.')
+      }
+
+      console.error(error)
     })
 }
 
